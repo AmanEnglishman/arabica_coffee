338345394ba35e45419fcd2e4af14c4aa5902bcd
AmanEnglishman <englishmanaman@gmail.com>
2026-05-10 14:09:43 +0600

    update parser send_code

--- apps/users/api/throttles.py
+++ apps/users/api/throttles.py
@@ -2,16 +2,18 @@ from rest_framework.throttling import AnonRateThrottle
 
 
 class SendCodeThrottle(AnonRateThrottle):
-    """3 запроса на отправку кода за 10 минут."""
+    """3 запроса на отправку SMS-кода за 10 минут с одного IP."""
     scope = "send_code"
 
-    def parse_rate(self, rate):
-        return 3, 600  # (num_requests, duration_seconds)
+    def __init__(self):
+        self.num_requests = 3
+        self.duration = 600  # 10 минут
 
 
 class VerifyCodeThrottle(AnonRateThrottle):
-    """5 попыток верификации за 10 минут."""
+    """5 попыток ввода кода за 10 минут с одного IP."""
     scope = "verify_code"
 
-    def parse_rate(self, rate):
-        return 5, 600
+    def __init__(self):
+        self.num_requests = 5
+        self.duration = 600  # 10 минут
